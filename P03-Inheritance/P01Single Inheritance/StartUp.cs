9bff34f9f33c108794b28baf25e82c816fd73496
iliyazz <iliyaz.softuni@gmail.com>
2022-06-30 01:14:57 +0300

    Inheritance - Lab, Single Inheritance

---
+++ P03-Inheritance/P01Single Inheritance/StartUp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Farm
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog();
+            dog.Bark();
+        }
+    }
+}

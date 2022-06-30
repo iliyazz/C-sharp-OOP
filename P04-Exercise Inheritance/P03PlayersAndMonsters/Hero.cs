a9524b0371a494c1d8a75f7986203a7f543b5d2e
iliyazz <iliyaz.softuni@gmail.com>
2022-06-30 23:09:57 +0300

    Inheritance - Exercise, Players and Monsters

---
+++ P04-Exercise Inheritance/P03PlayersAndMonsters/Hero.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlayersAndMonsters
+{
+    public class Hero
+    {
+        private string username;
+        private int level;
+
+        public string Username { get => username; set => username = value; }
+        public int Level { get => level; set => level = value; }
+
+        public Hero(string username, int level)
+        {
+            this.Username = username;
+            this.Level = level;
+        }
+        public override string ToString()
+        {
+            return $"Type: {this.GetType().Name} Username: {this.Username} Level: {this.Level}";
+        }
+    }
+}

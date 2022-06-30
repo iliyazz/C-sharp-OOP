971391426a1c2127f2b6001e9b5490a63d0f2245
iliyazz <iliyaz.softuni@gmail.com>
2022-06-30 22:19:58 +0300

    Inheritance - Lab, Random List

---
+++ P03-Inheritance/P04RandomList/RandomList.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        Random rand = new Random();
+        public string RandomString()
+        {
+            if (this.Count == 0)
+            {
+                return null;
+            }
+            var index = rand.Next(0, this.Count);
+            string element = this[index];
+            this.RemoveAt(index);
+            return element;
+        }
+    }
+}
---
+++ P03-Inheritance/P04RandomList/StartUp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CustomRandomList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            RandomList strings = new RandomList();
+            strings.Add("str1");
+            strings.Add("str2");
+            strings.Add("str3");
+            strings.Add("str4");
+            strings.Add("str5");
+            strings.RandomString();
+            strings.RandomString();
+            strings.RandomString();
+            strings.RandomString();
+            strings.RandomString();
+            strings.RandomString();
+            strings.RandomString();
+        }
+    }
+}

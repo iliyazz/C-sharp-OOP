9e9cdf1f4fbe91326f3cbfad36f8d397b83b8b01
iliyazz <iliyaz.softuni@gmail.com>
2022-06-30 22:21:02 +0300

    Inheritance - Lab, Stack of Strings

---
+++ P03-Inheritance/P05StackOfStrings/StackOfStrings.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomStack
+{
+    public class StackOfStrings : Stack<string>
+    {
+        Stack<string> stack = new Stack<string>();
+        public bool IsEmpty()
+        {
+            return this.Count == 0;
+        }
+        public Stack<string> AddRange(IEnumerable<string> elemennts)
+        {
+            foreach (var item in elemennts)
+            {
+                this.Push(item);
+            }
+            return this;
+        }
+    }
+}
---
+++ P03-Inheritance/P05StackOfStrings/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomStack
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>() { "word1", "word2", "word3"};
+            StackOfStrings stackOfStrings = new StackOfStrings();
+            Console.WriteLine(stackOfStrings.IsEmpty() );
+            stackOfStrings.AddRange(list);
+            Console.WriteLine(stackOfStrings.IsEmpty());
+        }
+    }
+}

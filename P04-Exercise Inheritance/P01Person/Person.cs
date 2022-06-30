a0ecbf61df3db1d2814b3b0592d03e155abec52c
iliyazz <iliyaz.softuni@gmail.com>
2022-06-30 22:24:31 +0300

    Inheritance - Exercise, Person

---
+++ P04-Exercise Inheritance/P01Person/Person.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Person
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public string Name { get => name; set => name = value; }
+
+        public virtual int Age
+        {
+            get
+            {
+                return age;
+            } 
+            set
+            {
+                if (value < 0)
+                {
+                    throw new Exception();
+                }
+                age = value;
+            }
+        }
+
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.Append($"Name: {this.Name}, Age: {this.Age}");
+            return sb.ToString().TrimEnd();
+        }
+    }
+}

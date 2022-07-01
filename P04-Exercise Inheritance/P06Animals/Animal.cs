e8953f8b0ee0a27054e46a95cd76242730d1f5d4
iliyazz <100124829+iliyazz@users.noreply.github.com>
2022-07-01 13:55:50 +0300

    Inheritance - Exercise, Animals

---
+++ P04-Exercise Inheritance/P06Animals/Animal.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public class Animal
+    {
+        private string name;
+        private int age;
+        private string gender;
+
+        public string Name { get => name; set => name = value; }
+        public int Age { get => age; set => age = value; }
+        public string Gender { get => gender; set => gender = value; }
+
+        public Animal(string name, int age, string gender)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Gender = gender;
+        }
+        public virtual string ProduceSound()
+        {
+            return null;
+        }
+    }
+}
---
+++ P04-Exercise Inheritance/P06Animals/StartUp.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Animals
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            while (true)
+            {
+                string animalType = Console.ReadLine();
+                if (animalType == "Beast!")
+                {
+                    break;
+                }
+                string[] tokens = Console.ReadLine().Split();
+                string name = tokens[0];
+                int age = int.Parse(tokens[1]);
+                if (age < 0)
+                {
+                    Console.WriteLine("Invalid input!");
+                    continue;
+                }
+                Animal animal = default;
+                if (animalType == "Dog")
+                {
+                    string gender = tokens[2];
+                    animal = new Dog(name, age, gender);
+                }
+                else if (animalType == "Cat")
+                {
+                    string gender = tokens[2];
+                    animal = new Cat(name, age, gender);
+                }
+                else if (animalType == "Frog")
+                {
+                    string gender = tokens[2];
+                    animal = new Frog(name, age, gender);
+                }
+                else if (animalType == "Kittens")
+                {
+                    animal = new Kitten(name, age);
+                }
+                else if(animalType == "Tomcat")
+                {
+                    animal = new Tomcat(name, age);
+                }
+                Console.WriteLine(animalType);
+                Console.WriteLine($"{animal.Name} {animal.Age} {animal.Gender}");
+                Console.WriteLine(animal.ProduceSound());
+            }
+        }
+    }
+}

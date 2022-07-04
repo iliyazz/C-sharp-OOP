348865d2257a5a72d71343eaeeeedfa71d351a95
iliyazz <iliyaz.softuni@gmail.com>
2022-07-05 00:17:47 +0300

    Encapsulation - Exercise, Class Box Data

---
+++ P06-Exercise Encapsulation/P01ClassBoxData/Box.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassBoxData
+{
+    public class Box
+    {
+        private double length;
+        private double width;
+        private double hieght;
+
+        public double Length
+        {
+            get{return this.length;}
+            private set
+            {
+                this.CheckValue(value, nameof(this.Length));
+                this.length = value;
+            } 
+        }
+        public double Width 
+        {
+            get { return this.width; }
+            private set
+            {
+                this.CheckValue(value, nameof(this.Width));
+                this.width = value;
+            }
+        }
+        public double Height 
+        {
+            get { return this.hieght; }
+            set
+            {
+                this.CheckValue(value, nameof(this.Height));
+                this.hieght = value;
+            }
+        }
+
+        public Box(double length, double width, double height)
+        {
+            Length = length;
+            Width = width;
+            Height = height;
+        }
+        private void CheckValue(double value, string dimenssion)
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentException($"{dimenssion} cannot be zero or negative.");
+            }
+        }
+        public double SurfaceArea()
+        {
+            return (this.Width * this.Height + this.Width * this.Length + this.Height * this.Length) * 2;
+        }
+        public double LateralSurfaceArea()
+        {
+            return (this.Length + this.Width) * 2 * this.Height;
+        }
+        public double Volume()
+        {
+            return this.Length * this.Height * this.Width;
+        }
+    }
+}
---
+++ P06-Exercise Encapsulation/P01ClassBoxData/StartUp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ClassBoxData
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var length = double.Parse(Console.ReadLine());
+            var width = double.Parse(Console.ReadLine());
+            var height = double.Parse(Console.ReadLine());
+            try
+            {
+                var box = new Box(length, width, height);
+                Console.WriteLine($"Surface Area - {box.SurfaceArea():f2}");
+                Console.WriteLine($"Lateral Surface Area - {box.LateralSurfaceArea():f2}");
+                Console.WriteLine($"Volume - {box.Volume():F2}");
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+        }
+    }
+}

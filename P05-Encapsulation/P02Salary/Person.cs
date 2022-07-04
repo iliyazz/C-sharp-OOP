36b132ade95f41b77870645089132ef65137f062
iliyazz <iliyaz.softuni@gmail.com>
2022-07-04 18:14:31 +0300

    Encapsulation - Lab, Salary

---
+++ P05-Encapsulation/P02Salary/Person.cs
@@ -0,0 +1,35 @@
+﻿namespace PersonsInfo
+{
+    public class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+        private decimal salary;
+
+        public string FirstName { get => firstName; private set => firstName = value; }
+        public string LastName { get => lastName; private set => lastName = value; }
+        public int Age { get => age; private set => age = value; }
+        public decimal Salary { get => salary; private set => salary = value; }
+
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            Salary = salary;
+        }
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} receives {Salary:f2} leva.";
+        }
+        public void IncreaseSalary(decimal percentage)
+        {
+            if (Age < 30)
+            {
+                percentage /= 2;
+            }
+            Salary *= 1 + percentage / 100;
+        }
+    }
+}

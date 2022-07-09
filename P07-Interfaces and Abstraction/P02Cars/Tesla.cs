b657ecfc95b9825dfc10b63d57a85fa0dd2dd6b6
iliyazz <iliyaz.softuni@gmail.com>
2022-07-09 12:32:28 +0300

    Interfaces and Abstraction - Lab, Cars

---
+++ P07-Interfaces and Abstraction/P02Cars/Tesla.cs
@@ -0,0 +1,31 @@
+﻿
+namespace Cars
+{
+    public class Tesla : IElectricCar, ICar
+    {
+        public int Battery { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+
+        public Tesla(string model, string color, int battery)
+        {
+            this.Battery = battery;
+            this.Model = model;
+            this.Color = color;
+        }
+
+        public string Start()
+        {
+            return "Engine start";
+        }
+
+        public string Stop()
+        {
+            return "Breaaak!";
+        }
+        public override string ToString()
+        {
+            return $"{this.Color} Tesla {this.Model} with {this.Battery} Batteries";
+        }
+    }
+}

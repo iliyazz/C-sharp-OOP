ff3c968fada9decb794a77f9d1596c471b3c8221
iliyazz <iliyaz.softuni@gmail.com>
2022-07-11 23:13:18 +0300

    Polymorphism - Lab, Shapes

---
+++ P09-Polymorphism/P03Shapes/Circle.cs
@@ -0,0 +1,29 @@
+﻿namespace Shapes
+{
+    using System;
+    public class Circle : Shape
+    {
+        private double radius;
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public double Radius { get => radius; private set => radius = value; }
+
+        public override double CalculateArea()
+        {
+            return Math.PI  * this.Radius * this.Radius;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return 2 * Math.PI * this.Radius;
+        }
+        public override string Draw()
+        {
+            return base.Draw() + " Circle";
+        }
+    }
+}
---
+++ P09-Polymorphism/P03Shapes/Rectangle.cs
@@ -0,0 +1,31 @@
+﻿namespace Shapes
+{
+    public class Rectangle : Shape
+    {
+        private double height;
+        private double width;
+
+        public Rectangle(int height, int width)
+        {
+            this.Height = height;
+            this.Width = width;
+        }
+
+        public double Height { get => height; private set => height = value; }
+        public double Width { get => width; private set => width = value; }
+
+        public override double CalculateArea()
+        {
+            return this.Height * this.Width;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return 2 * (this.Height + this.Width);
+        }
+        public override string Draw()
+        {
+            return base.Draw() + " Rectangle";
+        }
+    }
+}

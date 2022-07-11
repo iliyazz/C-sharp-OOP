76286c8c1b141c3dadb7f3c545df20a5b0c0cfaf
iliyazz <iliyaz.softuni@gmail.com>
2022-07-11 22:19:33 +0300

    Polymorphism - Lab, Animals

---
+++ P09-Polymorphism/P02Animals/Animal.cs
@@ -0,0 +1,21 @@
+﻿namespace Animals
+{
+    public class Animal
+    {
+        private string name;
+        private string favouriteFood;
+
+        public Animal(string name, string favouriteFood)
+        {
+            this.Name = name;
+            this.FavouriteFood = favouriteFood;
+        }
+
+        public string Name { get => name; protected set => name = value; }
+        public string FavouriteFood { get => favouriteFood; protected set => favouriteFood = value; }
+        public virtual string ExplainSelf()
+        {
+            return $"I am {this.Name} and my favourite food is {this.FavouriteFood}";
+        }
+    }
+}

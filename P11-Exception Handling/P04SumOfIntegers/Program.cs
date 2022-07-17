b74fa77a2ebc7c36cc7e415813037d69444860c0
iliyazz <iliyaz.softuni@gmail.com>
2022-07-17 14:38:27 +0300

    Exceptions and Error Handling - Lab, Sum of Integers

---
+++ P11-Exception Handling/P04SumOfIntegers/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace P04SumOfIntegers
+{
+    using System;
+    using System.Text.RegularExpressions;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputData = Console.ReadLine().Split();
+            long sum = 0;
+            foreach (var input in inputData)
+            {
+                try
+                {
+                    sum += SumIntegers(input, sum);
+                }
+                catch (ArgumentException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                finally
+                {
+                    Console.WriteLine($"Element '{input}' processed - current sum: {sum}");
+                }
+            }
+
+            Console.WriteLine($"The total sum of all integers is: {sum}");
+        }
+
+        private static int SumIntegers(string input, long sum)
+        {
+            if (!Regex.IsMatch(input, @"^[-]?[0-9]+$"))
+            {
+                throw new ArgumentException($"The element '{input}' is in wrong format!");
+            }
+
+            int inputInt;
+            bool isInteger = int.TryParse(input, out inputInt);
+            if (!isInteger)
+            {
+                throw new ArgumentException($"The element '{input}' is out of range!");
+            }
+
+            return inputInt;
+            
+        }
+    }
+}

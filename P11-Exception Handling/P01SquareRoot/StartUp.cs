38584d468c020196a212611c739c63d32eaea9a0
iliyazz <iliyaz.softuni@gmail.com>
2022-07-17 11:26:28 +0300

    Exceptions and Error Handling - Lab, Square Root

---
+++ P11-Exception Handling/P01SquareRoot/StartUp.cs
@@ -0,0 +1,34 @@
+﻿namespace P01SquareRoot
+{
+    using System;
+
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int number = int.Parse(Console.ReadLine());
+                
+                Console.WriteLine(Sqrt((double)number));
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Goodbye.");
+            }
+        }
+
+        public static double Sqrt(double number)
+        {
+            if (number < 0)
+            {
+                throw new ArgumentException("Invalid number.");
+            }
+            return Math.Sqrt(number);
+        }
+    }
+}

a5695d8a34844cc71df2284c1143daad8d64aa6a
iliyazz <iliyaz.softuni@gmail.com>
2022-07-24 12:11:17 +0300

    Reflection and Attributes - Lab, Mission Private Impossible

---
+++ P15-Reflection and Attributes/P03MissionPrivateImpossible/StartUp.cs
@@ -0,0 +1,19 @@
+﻿namespace Stealer
+{
+    using IO;
+    using IO.Contracts;
+    using Models;
+
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Spy spy = new Spy();
+            //string result = spy.StealFieldInfo("Stealer.Hacker", "username", "password");
+            //string result = spy.AnalyzeAccessModifiers("Stealer.Hacker");
+            string result = spy.RevealPrivateMethods("Stealer.Hacker");
+            IWriter writer = new ConsoleWriter();
+            writer.WriteLine(result);
+        }
+    }
+}

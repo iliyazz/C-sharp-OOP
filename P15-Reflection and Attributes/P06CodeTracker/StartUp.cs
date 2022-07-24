7ef8c8804831d0645c810fd36b466c467201ad61
iliyazz <iliyaz.softuni@gmail.com>
2022-07-24 12:45:30 +0300

    Reflection and Attributes - Lab, Code Tracker

---
+++ P15-Reflection and Attributes/P06CodeTracker/StartUp.cs
@@ -0,0 +1,15 @@
+﻿namespace AuthorProblem
+{
+    using System;
+    [Author("GeorgeI")]
+    [Author("GeorgeII")]
+    public class StartUp
+    {
+        [Author("GeorgeIV")]
+        static void Main()
+        {
+            var tracker = new Tracker();
+            tracker.PrintMethodsByAuthor();
+        }
+    }
+}
---
+++ P15-Reflection and Attributes/P06CodeTracker/Tracker.cs
@@ -0,0 +1,25 @@
+﻿namespace AuthorProblem
+{
+    using System;
+    using System.Reflection;
+
+    public class Tracker
+    {
+        public void PrintMethodsByAuthor()
+        {
+            Type type = typeof(StartUp);
+            foreach (var method in type.GetMethods((BindingFlags)60))
+            {
+                object[] attributeObjects = method.GetCustomAttributes(false);
+                foreach (var attributeObject in attributeObjects)
+                {
+                    AuthorAttribute authorAttribute = attributeObject as AuthorAttribute;
+                    if (authorAttribute != null)
+                    {
+                        Console.WriteLine($"{method.Name} is written by {authorAttribute.Name}");
+                    }
+                }
+            } 
+        }
+    }
+}

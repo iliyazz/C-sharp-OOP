3433a04aa898b74a239a130388577ea6dceb0aa9
iliyazz <iliyaz.softuni@gmail.com>
2022-07-27 20:16:06 +0300

    Unit Testing - Lab, Test Axe and Dummy Tests

---
+++ P17-Unit Testing/P01-Test Axe and Dummy/Skeleton.Tests/DummyTests.cs
@@ -0,0 +1,84 @@
+﻿using NUnit.Framework;
+
+namespace Skeleton.Tests
+{
+    using System;
+
+    [TestFixture]
+    public class DummyTests
+    {
+        [Test]
+        public void TestIfDummyLosesHealthIfAttacked()
+        {
+            //Arrange
+            int attackPoint = 20;
+            int dummyHealth = 50;
+            Dummy dummy = new Dummy(dummyHealth, 80);
+
+            //Act
+            dummy.TakeAttack(attackPoint);
+            int actualDummyhealth = dummy.Health;
+            int expectedDummyhealth = dummyHealth - attackPoint;
+
+            //Assert
+            Assert.AreEqual(expectedDummyhealth, actualDummyhealth, "Dummy should lose health if attacked");
+        }
+
+        [Test]
+        public void DeadDummyShouldThrowsAnExceptionIfAttacked()
+        {
+            int dummyHealth = 10;
+            int AttackPoint = 20;
+            //Arrange
+            Dummy dummy = new Dummy(dummyHealth, AttackPoint);
+
+            //Act
+            dummy.TakeAttack(AttackPoint);
+
+            //Assert
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                dummy.TakeAttack(AttackPoint);
+
+            }, "Dummy is dead.");
+
+        }
+        [TestCase()]
+        public void DeadDummyCanGiveXP()
+        {
+            //Arrange
+            int dummyHealth = 30;
+            int AttackPoint = 40;
+            int dummyExperience = 50;
+            
+            Dummy dummy = new Dummy(dummyHealth, dummyExperience);
+
+            //Act
+            dummy.TakeAttack(AttackPoint);
+
+            //Assert
+            Assert.AreEqual(dummyExperience, dummy.GiveExperience());
+        }
+        [Test]
+        public void AliveDummyCantGiveXP()
+        {
+            //Arrange
+
+            int dummyHealth = 30;
+            int AttackPoint = 20;
+            int dummyExperience = 50;
+            Dummy dummy = new Dummy(dummyHealth, dummyExperience);
+
+            //Act
+            //Assert
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                dummy.GiveExperience();
+
+            }, "Target is not dead.");
+        }
+
+
+
+    }
+}
--- P17-Unit Testing/P01-Test Axe and Dummy/Skeleton/StartUp.cs
+++ P17-Unit Testing/P01-Test Axe and Dummy/Skeleton/StartUp.cs
@@ -2,5 +2,7 @@
 {
     static void Main(string[] args)
     {
+        //Dummy dummy = new Dummy(10, 50);
+        //dummy.TakeAttack(50);
     }
 }

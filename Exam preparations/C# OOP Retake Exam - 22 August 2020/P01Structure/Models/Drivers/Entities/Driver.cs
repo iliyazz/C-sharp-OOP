d59c1c6d6b2b7bcc379af7384c48c271a15d107e
iliyazz <100124829+iliyazz@users.noreply.github.com>
2022-08-12 08:32:49 +0300

    C# OOP Retake Exam - 22 August 2020, Structure

---
+++ Exam preparations/C# OOP Retake Exam - 22 August 2020/P01Structure/Models/Drivers/Entities/Driver.cs
@@ -0,0 +1,70 @@
+﻿namespace EasterRaces.Models.Drivers.Entities
+{
+    using System;
+    using Cars.Contracts;
+    using Contracts;
+    using Utilities.Messages;
+
+    public class Driver : IDriver
+    {
+        private string name;
+        private ICar car;
+        private int numberOfWins;
+        private bool canParticipate = false;
+
+        public Driver(string name)
+        {
+            Name = name;
+        }
+
+
+
+        public string Name
+        {
+            get => name;
+            private set
+            {
+                if (string.IsNullOrEmpty(value) || value.Length < 5)
+                {
+                    throw new ArgumentException(string.Format(ExceptionMessages.InvalidName, value, 5));
+                }
+                name = value;
+            }
+        }
+
+        public ICar Car
+        {
+            get => car;
+            private set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException(ExceptionMessages.CarInvalid);
+                }
+                car = value;
+            }
+        }
+
+        public int NumberOfWins
+        {
+            get => numberOfWins;
+            private set => numberOfWins = value;
+        }
+
+        public bool CanParticipate
+        {
+            get => canParticipate;
+            private set => canParticipate = value;
+        }
+        public void WinRace()
+        {
+            numberOfWins++;
+        }
+
+        public void AddCar(ICar car)
+        {
+            Car = car;
+            canParticipate = true;
+        }
+    }
+}
---
+++ Exam preparations/C# OOP Retake Exam - 22 August 2020/P01Structure/Repositories/Entities/Repository.cs
@@ -0,0 +1,25 @@
+﻿namespace EasterRaces.Repositories.Entities
+{
+    using System.Collections.Generic;
+    using Contracts;
+
+    public abstract class Repository<T> : IRepository<T> where T :class
+
+    {
+        private ICollection<T> models;
+
+        protected Repository()
+        {
+            models = new List<T>();
+        }
+        public ICollection<T> Models => models;
+
+        public abstract T GetByName(string name);
+
+        public IReadOnlyCollection<T> GetAll() => this.models as IReadOnlyCollection<T>;
+
+        public void Add(T model) => models.Add(model);
+
+        public bool Remove(T model) => models.Remove(model);
+    }
+}

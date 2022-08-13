1b0bad8d2ce079cd9f78e64c0f337559782bee58
iliyazz <iliyaz.softuni@gmail.com>
2022-08-13 15:03:00 +0300

    C# OOP Exam - 16 August 2020, Business Logic

---
+++ Exam preparations/C# OOP Exam - 16 August 2020/P02BusinessLogic/Core/Controller.cs
@@ -0,0 +1,187 @@
+﻿namespace OnlineShop.Core
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Common.Constants;
+    using Common.Enums;
+    using Models.Products.Components;
+    using Models.Products.Computers;
+    using Models.Products.Peripherals;
+
+    public class Controller : IController
+    {
+        private ICollection<IComputer> computers;
+        private ICollection<IPeripheral> peripherals;
+        private ICollection<IComponent> components;
+
+        public Controller()
+        {
+            computers = new List<IComputer>();
+            peripherals = new List<IPeripheral>();
+            components = new List<IComponent>();
+        }
+        public string AddComputer(string computerType, int id, string manufacturer, string model, decimal price)
+        {
+            IComputer computer = null;
+            if (computers.Any(x => x.Id == id))
+            {
+                throw new ArgumentException(ExceptionMessages.ExistingComputerId);
+            }
+
+            if (computerType == nameof(DesktopComputer))
+            {
+                computer = new DesktopComputer(id, manufacturer, model, price);
+            }
+            else if (computerType == nameof(Laptop))
+            {
+                computer = new Laptop(id, manufacturer, model, price);
+            }
+            else
+            {
+                throw new ArgumentException(ExceptionMessages.InvalidComputerType);
+            }
+            computers.Add(computer);
+            return string.Format(SuccessMessages.AddedComputer, id);
+        }
+
+        public string AddComponent(int computerId, int id, string componentType, string manufacturer, string model,
+            decimal price,
+            double overallPerformance, int generation)
+        {
+            if (components.Any(c => c.Id == id))
+            {
+                throw new ArgumentException(ExceptionMessages.ExistingComponentId);
+            }
+            IComponent component = null;
+            if (componentType == nameof(ComponentType.CentralProcessingUnit))
+            {
+                component = new CentralProcessingUnit(id, manufacturer, model, price, overallPerformance, generation);
+            }
+            else if (componentType == nameof(ComponentType.Motherboard))
+            {
+                component = new Motherboard(id, manufacturer, model, price, overallPerformance, generation);
+            }
+            else if (componentType == nameof(ComponentType.PowerSupply))
+            {
+                component = new PowerSupply(id, manufacturer, model, price, overallPerformance, generation);
+            }
+            else if (componentType == nameof(ComponentType.RandomAccessMemory))
+            {
+                component = new RandomAccessMemory(id, manufacturer, model, price, overallPerformance, generation);
+            }
+            else if (componentType == nameof(ComponentType.SolidStateDrive))
+            {
+                component = new SolidStateDrive(id, manufacturer, model, price, overallPerformance, generation);
+            }
+            else if (componentType == nameof(ComponentType.VideoCard))
+            {
+                component = new VideoCard(id, manufacturer, model, price, overallPerformance, generation);
+            }
+            else
+            {
+                throw new ArgumentException(ExceptionMessages.InvalidComponentType);
+            }
+
+            IComputer computer = IfComputerExist(computerId);
+            computer.AddComponent(component);
+            components.Add(component);
+            return string.Format(SuccessMessages.AddedComponent, componentType, id, computerId);
+        }
+
+        public string RemoveComponent(string componentType, int computerId)
+        {
+            IComputer computer = IfComputerExist(computerId);
+            IComponent component = computer.RemoveComponent(componentType);
+            components.Remove(component);
+            return string.Format(SuccessMessages.RemovedComponent, componentType, component.Id);
+        }
+
+        public string AddPeripheral(int computerId, int id, string peripheralType, string manufacturer, string model, decimal price,
+            double overallPerformance, string connectionType)
+        {
+            if (peripherals.Any(p => p.Id == id))
+            {
+                throw new ArgumentException(ExceptionMessages.ExistingPeripheralId);
+            }
+
+            IPeripheral peripheral = null;
+            if (peripheralType == nameof(Headset))
+            {
+                peripheral = new Headset(id, manufacturer, model, price, overallPerformance, connectionType);
+            }
+            else if (peripheralType == nameof(Keyboard))
+            {
+                peripheral = new Keyboard(id, manufacturer, model, price, overallPerformance, connectionType);
+            }
+            else if (peripheralType == nameof(Monitor))
+            {
+                peripheral = new Monitor(id, manufacturer, model, price, overallPerformance, connectionType);
+            }
+            else if (peripheralType == nameof(Mouse))
+            {
+                peripheral = new Mouse(id, manufacturer, model, price, overallPerformance, connectionType);
+            }
+            else
+            {
+                throw new ArgumentException(ExceptionMessages.InvalidPeripheralType);
+            }
+            IComputer computer = IfComputerExist(computerId);
+            computer.AddPeripheral(peripheral);
+            peripherals.Add(peripheral);
+            return string.Format(SuccessMessages.AddedPeripheral, peripheralType, id, computerId);
+        }
+
+        public string RemovePeripheral(string peripheralType, int computerId)
+        {
+            IComputer computer = IfComputerExist(computerId);
+            IPeripheral peripheral = computer.RemovePeripheral(peripheralType);
+            peripherals.Remove(peripheral);
+            return string.Format(SuccessMessages.RemovedPeripheral, peripheralType, peripheral.Id);
+        }
+
+        public string BuyComputer(int id)
+        {
+            IComputer computer = IfComputerExist(id);
+            computers.Remove(computer);
+            return computer.ToString();
+        }
+
+        public string BuyBest(decimal budget)
+        {
+            var orderedByPerformance = computers.OrderByDescending(c => c.OverallPerformance);
+            IComputer bestComputer = null;
+            foreach (var computer in orderedByPerformance)
+            {
+                if (computer.Price <= budget)
+                {
+                    bestComputer = computer;
+                    break;
+                }
+            }
+            if (bestComputer == null)
+            {
+                throw new ArgumentException(string.Format(ExceptionMessages.CanNotBuyComputer, budget));
+            }
+            computers.Remove(bestComputer);
+            return bestComputer.ToString();
+        }
+
+        public string GetComputerData(int id)
+        {
+            IComputer computer = IfComputerExist(id);
+            return computer.ToString();
+        }
+
+        private IComputer IfComputerExist(int id)
+        {
+            return computers.All(c => c.Id != id)
+                ? throw new ArgumentException(ExceptionMessages.NotExistingComputerId)
+                : computers.First(c => c.Id == id);
+        }
+
+
+
+    }
+}
---
+++ Exam preparations/C# OOP Exam - 16 August 2020/P02BusinessLogic/Models/Products/Computers/Laptop.cs
@@ -0,0 +1,9 @@
+﻿namespace OnlineShop.Models.Products.Computers
+{
+    public class Laptop : Computer
+    {
+        public Laptop(int id, string manufacturer, string model, decimal price) : base(id, manufacturer, model, price, 10)
+        {
+        }
+    }
+}
